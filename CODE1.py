a63679b734d997daaa8b7aea0e13d1e5d60b7e20
VishritVivek <vivshi654@yahoo.co.in>
2025-04-14 01:07:12 +0530

    Create CODE1.py

---
+++ CODE1.py
@@ -0,0 +1,13 @@
+from math import log
+from math import sin
+from math import sqrt
+num=float(input("ENTER YOUR NUMBER : "))
+def square(n):
+    print("SQUARE ROOT IS : ",(sqrt(n)))
+def loga(m):
+    print("NATURAL LOGARITHM IS : ",(log(m)))
+def sine(o):
+    print("SINE IS : ",(sin(o)))
+a=square(num)
+b=loga(num)
+c=sine(num)
